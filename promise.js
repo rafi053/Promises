4b4aa739c056e07f9f933a639b696cb21fa47a00
rafi053 <rafi053313@gmail.com>
2024-09-15 13:16:19 +0300

    I finished successfully

--- promise.js
+++ promise.js
@@ -1,11 +1,11 @@
 // 1
-// const successPromise = () =>{
-//     let promise = new Promise((resolve, reject) => {
-//         resolve("Success!");
-//     })
-//     return promise
+const successPromise = () =>{
+    let promise = new Promise((resolve, reject) => {
+        resolve("Success!");
+    })
+    return promise
 
-// };
+};
 
 // successPromise()
 //     .then((res) => console.log(res))
@@ -13,16 +13,16 @@
 
 
 // 2
-// const getFname = (firstName, lastName) => {
-//     let fName = `${firstName + " " + lastName}`;
-//     let promise = new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve(fName);
-//         }, 1000)
-//     })
-//     return promise
+const getFname = (firstName, lastName) => {
+    let fName = `${firstName + " " + lastName}`;
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(fName);
+        }, 1000)
+    })
+    return promise
 
-// };
+};
 
 // getFname("da", "le")
 //     .then((res) => console.log(res))
@@ -31,7 +31,7 @@
 
 // 3
 const addFive = (num) => {
-    let promise = new Promise((resolve, reject) => {
+    let promise = new Promise((resolve) => {
         setTimeout(() => {
         resolve(num + 5);
     }, 500)
@@ -39,7 +39,7 @@ const addFive = (num) => {
     return promise
 };
 const multiplyByTwo = (num) => {
-    let promise = new Promise((resolve, reject) => {
+    let promise = new Promise((resolve) => {
         setTimeout(() => {
         resolve(num * 2);
     }, 500)
@@ -47,7 +47,7 @@ const multiplyByTwo = (num) => {
     return promise
 };
 const subtractTen = (num) => {
-    let promise = new Promise((resolve, reject) => {
+    let promise = new Promise((resolve) => {
         setTimeout(() => {
         resolve(num - 10);
     }, 500)
@@ -56,11 +56,68 @@ const subtractTen = (num) => {
 };
 
 
-const result = addFive(10)
-    .then(numFive =>  multiplyByTwo(numFive))
-    .then(numMulti => multiplyByTwo(numMulti))
-    .then(nomSub => subtractTen(nomSub))
-    .then(res => console.log(res)
-    )
+// addFive(10)
+//     .then(numFive =>  multiplyByTwo(numFive))
+//     .then(nomSub => subtractTen(nomSub))
+//     .then(res => console.log(res))
 
 
+// 4
+const divide = (num1, num2) => {
+    let promise = new Promise((resolve, reject) => {
+        if (num2 == 0){
+            reject("Cannot be divided by zero")
+        }
+        setTimeout(() => {
+        resolve(num1 / num2);
+    }, 500)
+    })
+    return promise
+};
+
+// divide(10, 0)
+//     .then((res) => console.log(res))
+//     .catch((e) => console.log(e))
+
+
+// 5A
+async function getFnameAsyncOperations() {
+    try {
+    const result = await getFname("da", "le");
+    console.log(result);
+    
+    } catch (error) {
+    console.error(error);
+    }
+   };
+
+// getFnameAsyncOperations("da", "le");
+
+
+// 5B
+async function ArithmeticAsyncOperations() {
+    try {
+        const numFive = await addFive(10)
+        const numMulti = await multiplyByTwo(numFive);
+        const nomSub = await subtractTen(numMulti);
+        await console.log(nomSub);
+        
+    } catch (error) {
+    console.error(error);
+    }
+   };
+
+// ArithmeticAsyncOperations()
+
+
+// 6
+async function divideAsyncOperations() {
+    try {
+    const result = await divide(10, 5);
+    console.log(result);
+    
+    } catch (error) {
+    console.error(error);
+    }
+   };
+// divideAsyncOperations();
